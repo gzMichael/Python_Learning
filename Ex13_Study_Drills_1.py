c200d3c3c58b4ee04e879879a59215800678ddd1
gzMichael <1960809@qq.com>
2016-12-23 13:55:32 +0800

    Add files via upload

---
+++ Ex13_Study_Drills_1.py
@@ -0,0 +1,5 @@
+from sys import argv
+script, firstname, lastname = argv
+print ("The script is called:", script )
+print ("Your first name is:", firstname )
+print ("Your last name is:", lastname )
---
+++ Ex13_Study_Drills_2.py
@@ -0,0 +1,7 @@
+from sys import argv
+script, Alpha, Bravo, Charlie , Delta = argv
+print ("The script is called:", script )
+print ("Your Alpha variable is:", Alpha )
+print ("Your Bravo variable is:", Bravo )
+print ("Your Charlie variable is:", Charlie)
+print ("Your Delta variable is:", Delta)
