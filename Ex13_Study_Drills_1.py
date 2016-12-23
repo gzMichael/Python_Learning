6b2163a5a6c4147a9058b6d642aa5518eb042b30
gzMichael <1960809@qq.com>
2016-12-23 14:46:59 +0800

    Update

--- Ex13_Study_Drills_1.py
+++
@@ -1,5 +0,0 @@
-from sys import argv
-script, firstname, lastname = argv
-print ("The script is called:", script )
-print ("Your first name is:", firstname )
-print ("Your last name is:", lastname )
--- README.md
+++
@@ -1,2 +0,0 @@
-# Python_Learning
-Here are the exercise files for 《Learn Python The Hard Way》 
